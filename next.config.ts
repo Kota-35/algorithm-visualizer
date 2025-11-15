7029b690a1911dc176ff6fa7fce7d1e5196ecd16
Kota-35 <ishikuro6.2@gmail.com>
2025-11-15 11:19:17 +0900

    refactor: next.config.ts の webpack 設定から asyncWebAssembly のサポートを削除

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.experiments = {
-        ...config.experiments,
-        asyncWebAssembly: true,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
